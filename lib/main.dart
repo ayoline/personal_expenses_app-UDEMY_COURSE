32b4cb459d823f621c2e3be7afcfc58e19fd0c6b
Anderson P Soares <ayo@outlook.com.br>
2023-03-11 15:16:13 -0300

    add transaction with a date

--- lib/main.dart
+++ lib/main.dart
@@ -58,12 +58,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String txTitle, double txAmount) {
+  void _addNewTransaction(String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
       title: txTitle,
       amount: txAmount,
-      date: DateTime.now(),
+      date: chosenDate,
     );
 
     setState(() {
